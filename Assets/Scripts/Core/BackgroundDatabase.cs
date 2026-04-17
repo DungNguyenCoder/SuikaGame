50d29849c0fada4be45245894a961389a470bfb6
DungNguyenCoder <dungnickchinh1909@gmail.com>
2026-04-17 17:19:14 +0700

    Update: database

---
+++ Assets/Scripts/Core/BackgroundDatabase.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core
+{
+    [CreateAssetMenu(menuName = "SuikaGame/Data/Background Picture")]
+    public class BackgroundDatabase : ScriptableObject
+    {
+        public List<BackgroundData> backgroundPictures = new List<BackgroundData>();
+
+        public Sprite GetBackgroundSprite(int backgroundID)
+        {
+            var backgroundPicture = backgroundPictures.Find(b => b.ID == backgroundID);
+            return backgroundPicture != null ? backgroundPicture.Sprite : null;
+        }
+    }
+}
