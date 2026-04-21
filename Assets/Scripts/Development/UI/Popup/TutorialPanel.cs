32407f3b7fa8c5c4fcb7cf26e68bb6a8d16cd366
DungNguyenCoder <dungnickchinh1909@gmail.com>
2026-04-21 10:52:22 +0700

    Update: Tutorial panel and animation

--- Assets/Scripts/Development/UI/Popup/TutorialPanel.cs
+++ Assets/Scripts/Development/UI/Popup/TutorialPanel.cs
@@ -1,7 +1,19 @@
+using Development.Managers;
+using Development.Utils;
+using UnityEngine.SceneManagement;
+
 namespace Development.UI.Popup
 {
     public class TutorialPanel : Panel
     {
-        
+        public void OnClickStartGame()
+        {
+            SceneManager.LoadScene(GameConfig.GAMEPLAY_SCENE);
+        }
+
+        public void OnClickGoBack()
+        {
+            PanelManager.Instance.ClosePanel(PanelConfig.TUTORIAL_PANEL);
+        }
     }
 }
